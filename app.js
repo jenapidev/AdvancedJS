cb6e42860f21f986c5fc3d2a2edf4b698f5af285
jenapidev <jeanpierrenieto@gmail.com>
2019-01-17 21:09:19 -0400

    ui controller changes

--- app.js
+++ app.js
@@ -63,7 +63,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -74,6 +76,28 @@ var UIController = (function () {
                 value:  document.querySelector(DOMStrings.inputValue).value
             }; 
         },
+
+        addListItem: function(obj, type) {
+            var html, newHTML;
+            //create html string with placeholders tags
+
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"></i></button></div> </div> </div>';
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%%description</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';  
+            }
+            
+            
+
+            //replace the placeholder text with some data
+            newHTML = html.replace('%id%', obj.id);
+            newHTML = newHTML.replace('%description%', obj.description);
+            newHTML = newHTML.replace('%value%', obj.value);
+
+            //insert the html into the DOM
+            
+        },
+
         getDOMStrings: function() {
             return DOMStrings;
         }
